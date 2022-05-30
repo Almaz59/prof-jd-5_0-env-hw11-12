6434a4e64a2cbc5cc2abb206276e4ca4d4882be8
Almaz59 <100568607+Almaz59@users.noreply.github.com>
2022-05-30 18:03:00 +0300

    hw11_12 first commit

---
+++ src/main/java/pro/sky/java/course2/profjd5_0envhw1112/ProfJd50EnvHw1112Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.profjd5_0envhw1112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfJd50EnvHw1112Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfJd50EnvHw1112Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/profjd5_0envhw1112/ProfJd50EnvHw1112ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.profjd5_0envhw1112;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfJd50EnvHw1112ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
